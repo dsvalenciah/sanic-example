ba6d16a60080c832e4766e9d4d499d1f1d88b2e5
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-02-10 16:54:32 -0500

    Add password hashing support

--- CRUD.py
+++ CRUD.py
@@ -1,25 +1,36 @@
-#bultin library
+# bultin library
 
-#external libraries
+# external libraries
+from pony.orm import select
 import pony.orm as pony
 from models import User
 from pprint import pprint
-import json
+import hashlib
+
+
+SALT = 'aslkdfjlaskdjflkhlñwerhl1234khl2kjrlkjlkejrlkwjer'
+
+
+def hash_password(password):
+    return hashlib.md5((SALT + password).encode('utf-8')).hexdigest()
+
 
 @pony.db_session
 def add_user(name, password):
     new_user = User(
-        name = name,
-        password = password,
+        name=name,
+        password=hash_password(password),
     )
+    return new_user
+
 
 @pony.db_session
 def find_user(name, password):
-    result = User.get(
-        name=name,
-        password=password,
-    )
-    pprint(json.loads(str(result)))
-    return result
+    user = select(
+        u for u in User
+        if u.name == name and u.password == hash_password(password)
+    ).first()
+    pprint(user)
+    return user
 
 # encoding alternativo
