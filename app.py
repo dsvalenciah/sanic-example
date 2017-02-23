df6056177be0609fefeb36b5e60b92f455872a30
dsvalenciah <dsvalenciah@unal.edu.co>
2017-02-23 13:08:18 -0500

    Agregado comentario '¿como asignar un nombre de dominio?'

--- app.py
+++ app.py
@@ -61,6 +61,18 @@ async def login(request):
 if __name__ == "__main__":
     app.run(
         debug=True,
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=8000
     )
+
+'''
+    Para crear un dominio al estilo www.app.dev
+    y así ingresar a la app se debe cambiar
+    el archivo /etc/hosts en el cual se debe agregar
+    una linea asi '127.0.0.1 www.app.dev' luego,
+    se debe redirigir el trafico del puerto 8000
+    (en este caso) al puerto 80 (puerto por defecto)
+    de esta forma:
+    sudo iptables -t nat -A OUTPUT -o lo -p tcp
+    --dport 80 -j REDIRECT --to-port 6969
+'''
