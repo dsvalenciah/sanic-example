a19c6ef49086148c73db4d08eb2c7d7e95a69126
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-02-24 15:51:29 -0500

    Organiza alguna documentación

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sanic-example
 Example using sanic, pony orm and more.
+
+Para crear un dominio al estilo www.app.dev y así ingresar a la app se debe
+cambiar el archivo /etc/hosts en el cual se debe agregar una linea asi
+'127.0.0.1 www.app.dev' luego, se debe redirigir el trafico del puerto 8000 (en
+este caso) al puerto 80 (puerto por defecto) de esta forma:
+
+```
+sudo iptables -t nat -A OUTPUT -o lo -p tcp \
+    --dport 80 -j REDIRECT --to-port 6969
+```
--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-# bultin library
-import pprint
-
 # external libraries
 from sanic import Sanic
 from sanic.response import html, redirect
@@ -64,15 +61,3 @@ if __name__ == "__main__":
         host="127.0.0.1",
         port=8000
     )
-
-'''
-    Para crear un dominio al estilo www.app.dev
-    y así ingresar a la app se debe cambiar
-    el archivo /etc/hosts en el cual se debe agregar
-    una linea asi '127.0.0.1 www.app.dev' luego,
-    se debe redirigir el trafico del puerto 8000
-    (en este caso) al puerto 80 (puerto por defecto)
-    de esta forma:
-    sudo iptables -t nat -A OUTPUT -o lo -p tcp
-    --dport 80 -j REDIRECT --to-port 6969
-'''
